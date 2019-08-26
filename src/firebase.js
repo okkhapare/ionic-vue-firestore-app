3b6de0a7de1b428b26e0c9da948dee91ce94ea24
iskay <okkhapare@gmail.com>
2019-08-26 18:46:14 +0530

    structure created + dependencies installed

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDyhK4NepdfjKudcTkrP3yl8hftJXsVd1c",
+    authDomain: "ionic-vue-firestore-app.firebaseapp.com",
+    databaseURL: "https://ionic-vue-firestore-app.firebaseio.com",
+    projectId: "ionic-vue-firestore-app",
+    storageBucket: "ionic-vue-firestore-app.appspot.com",
+    messagingSenderId: "798882780307",
+    appId: "1:798882780307:web:bb09f94f1c22a87e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
+import IonicVue from '@ionic/vue'
+import '@ionic/core/css/core.css'
+import '@ionic/core/css/ionic.bundle.css'
 
 Vue.config.productionTip = false
+
+Vue.use(IonicVue)
 
 new Vue({
   router,
